85b9dfb49f45e42d8657db88d9d5da04a3f3fd1e
Luis Felipe Morales <145800266+LMORALESDEV@users.noreply.github.com>
2025-07-23 19:57:22 -0400

    Create PaisViewModel.cs

---
+++ AgenciaDeTours/Models/PaisViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgenciaDeTours.Models
+{
+    public class PaisViewModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
