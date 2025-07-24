e3c993526bba623ba7e3d780ab9d68cc11c651a5
Luis Felipe Morales <145800266+LMORALESDEV@users.noreply.github.com>
2025-07-24 15:27:10 -0400

    Create AgenciaDeToursDbContext.cs

---
+++ AgenciaDeTours/Datos/AgenciaDeToursDbContext.cs
@@ -0,0 +1,18 @@
+﻿using AgenciaDeTours.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgenciaDeTours.Datos
+{
+    public class AgenciaDeToursDbContext : DbContext
+    {
+        public AgenciaDeToursDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected AgenciaDeToursDbContext()
+        {
+        }
+
+        public DbSet<PaisViewModel> Paises { get; set; }
+    }
+}
