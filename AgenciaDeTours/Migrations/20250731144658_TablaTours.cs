8ba4f1e98059531365f56f9e83df7926fd807e24
Luis Felipe Morales <145800266+LMORALESDEV@users.noreply.github.com>
2025-08-04 11:39:25 -0400

    Ajuste de tablas para captura de datos

---
+++ AgenciaDeTours/Migrations/20250731144658_TablaTours.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AgenciaDeTours.Migrations
+{
+    /// <inheritdoc />
+    public partial class TablaTours : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tours",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DestinoId = table.Column<int>(type: "int", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Precio = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ITBIS = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DuracionDias = table.Column<int>(type: "int", nullable: false),
+                    DuracionHoras = table.Column<int>(type: "int", nullable: false),
+                    FechaFin = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EstadoTour = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tours", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tours_Destinos_DestinoId",
+                        column: x => x.DestinoId,
+                        principalTable: "Destinos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tours_DestinoId",
+                table: "Tours",
+                column: "DestinoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tours");
+        }
+    }
+}
---
+++ AgenciaDeTours/Migrations/20250731185808_ColumnaEstadoEsCalculada.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AgenciaDeTours.Migrations
+{
+    /// <inheritdoc />
+    public partial class ColumnaEstadoEsCalculada : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "EstadoTour",
+                table: "Tours",
+                type: "int",
+                nullable: false,
+                computedColumnSql: "CASE WHEN [Fecha] > CAST(GETDATE() AS date) THEN 1 ELSE 2 END",
+                stored: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "EstadoTour",
+                table: "Tours",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldComputedColumnSql: "CASE WHEN [Fecha] > CAST(GETDATE() AS date) THEN 1 ELSE 2 END");
+        }
+    }
+}
