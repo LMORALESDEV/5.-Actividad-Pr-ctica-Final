a4b887fef56bd8c853eb2d3af5ba2ec665ad4c4d
Luis Felipe Morales <145800266+LMORALESDEV@users.noreply.github.com>
2025-08-04 11:24:44 -0400

    Actuzalicion de conexion dbcontext

--- AgenciaDeTours/Datos/AgenciaDeToursDbContext.cs
+++ AgenciaDeTours/Datos/AgenciaDeToursDbContext.cs
@@ -13,6 +13,21 @@ namespace AgenciaDeTours.Datos
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<TourViewModel>()
+                   .Property(e => e.EstadoTour)
+                   .HasComputedColumnSql(
+                       "CASE WHEN [Fecha] > CAST(GETDATE() AS date) THEN 1 ELSE 2 END",
+                       stored: false 
+                   );
+
+        }
+
         public DbSet<PaisViewModel> Paises { get; set; }
+        public DbSet<DestinoViewModel> Destinos { get; set; }
+        public DbSet<TourViewModel> Tours { get; set; }
     }
 }
